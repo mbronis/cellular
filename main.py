705aafc01513722d7919b8720d678bb78abde6ac
mbronis <michal.bronis@gmail.com>
2022-01-27 23:25:48 +0100

    cave smoothing

--- main.py
+++ main.py
@@ -8,8 +8,12 @@ import numpy as np
 import pygame
 from pygame.locals import QUIT, MOUSEBUTTONDOWN
 
-from src.settings import WIDTH, HEIGHT, TILESIZE, TITLE
 
+TITLE = 'A cellular automata game'
+WIDTH = 60
+HEIGHT = 40
+TILESIZE = 15
+FPS = 10
 
 
 class Cave:
@@ -17,11 +21,19 @@ class Cave:
         self.fill = fill
         self.seed = seed
 
-        self.map: np.ndarray = self.generate_map()
+        self.map = self.generate_map()
+        self.rect = self.generate_map_rect()
         self.fill_map_random()
     
     def generate_map(self) -> np.ndarray:
         return np.empty((WIDTH, HEIGHT))
+
+    def generate_map_rect(self):
+        rect = dict()
+        for x, y in itertools.product(range(WIDTH), range(HEIGHT)):
+            rect[(x, y)] = get_cell_rect(x, y)
+        
+        return rect
 
     def fill_map_random(self):
         if self.seed:
@@ -34,12 +46,24 @@ class Cave:
             self.map[x, y] = 1 if random.random() < self.fill else 0
     
     def smooth_map(self):
-        pass
+        for x, y in itertools.product(range(WIDTH), range(HEIGHT)):
+            if (x == 0) or (x == WIDTH-1) or (y == 0) or (y == HEIGHT-1):
+                continue
+
+            walls_count = self.surrounding_wall_count(x, y)
+            if walls_count > 4:
+                self.map[x, y] = 1
+            elif walls_count < 4:
+                self.map[x, y] = 0
 
     def surrounding_wall_count(self, x, y):
-        pass        
-        # for x, y in itertools.product(range(WIDTH), range(HEIGHT)):
-
+        walls_count = 0
+        for nx, ny in itertools.product(range(x-1, x+2), range(y-1, y+2)):
+            if (nx < 0) or (nx > WIDTH-1) or (ny < 0) or (ny > HEIGHT-1):
+                continue
+            walls_count += self.map[nx, ny]
+        
+        return walls_count
 
 def draw_cave(screen, cave):
     colors = {
@@ -47,7 +71,7 @@ def draw_cave(screen, cave):
         0: 'ivory',
     }
     for x, y in itertools.product(range(WIDTH), range(HEIGHT)):
-        screen.fill(colors[cave.map[x, y]], get_cell_rect(x, y))
+        screen.fill(colors[cave.map[x, y]], cave.rect[(x, y)])
 
 def get_cell_rect(x, y):
     return pygame.Rect(x * TILESIZE, y * TILESIZE, TILESIZE, TILESIZE)
@@ -57,19 +81,32 @@ def reset_cave(screen):
     draw_cave(screen, cave)
     pygame.display.update()
 
+    return cave
+
+def smooth_cave(screen, cave):
+    cave.smooth_map()
+    draw_cave(screen, cave)
+    pygame.display.update()
+
 def main():
     pygame.init()
 
     screen = pygame.display.set_mode((WIDTH * TILESIZE, HEIGHT * TILESIZE))
     pygame.display.set_caption(TITLE)
+    
+    clock = pygame.time.Clock()
 
-    reset_cave(screen)
+    cave = reset_cave(screen)
     while True:
+        clock.tick(FPS)
+
         for event in pygame.event.get():
             if event.type == QUIT:
                 return
             elif event.type == MOUSEBUTTONDOWN:
-                reset_cave(screen)
+                cave = reset_cave(screen)
+
+        smooth_cave(screen, cave)
 
 if __name__ == '__main__':
     main()
--- src/settings.py
+++
@@ -1,7 +0,0 @@
-TITLE = 'A cellular automata game'
-
-WIDTH = 60
-HEIGHT = 40
-TILESIZE = 15
-
-FPS = 60
